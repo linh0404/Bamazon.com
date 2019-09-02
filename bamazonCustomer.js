db58a2d83cf2d9958c21f9410763588d954e2a5a
Linh Nguyen <linh0404@hotmail.com>
2019-09-02 13:32:21 +1000

    added inquirer package and displayed table in console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require ("mysql");
+var inquirer = require ("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,13 +13,64 @@ connection.connect(function(err){
     if(err) throw err;
     console.log("connected as id " + connection.threadId);
     queryAllItems();
+    start();
 });
 
 function queryAllItems() {
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity)
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price)
         }
     })
-}
+};
+
+function start() {
+  connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+    inquirer.prompt([
+        {
+            name: "chosenItemId",
+            type: "rawlist",
+            choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].product_name);
+                }
+                return choiceArray;
+            },
+            message: "What item would you like to buy? (Please state the item_id no.)"
+        },
+        {
+            name: "howMuchToBuy",
+            type: "input",
+            message: "How many would you like to buy?"
+        },
+    ])
+    .then(function(answer) {
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name === answer.chosenItemId) {
+                chosenItem = res[i];
+            }
+        }
+        if (chosenItem.stock_quantity < res[i].answer.howMuchToBuy) {
+            connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: stock_quantity - answer.howMuchToBuy
+                    },
+                    {
+                        item_id: chosenItem
+                    }
+                ],
+                function(error) {
+                    if (error) throw err;
+                    console.log("Sorry, we have insufficient inventory at the moment");
+                    start();
+                }
+            )
+        }
+    })
+})};
+
